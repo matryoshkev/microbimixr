27894a862a96a83baed2c7c67db38b55ba6e99bd
jeff smith <matryoshkev@gmail.com>
2026-09-10 07:57:12 -0500

    Add tests for plot args

--- tests/testthat/test-axes.R
+++ tests/testthat/test-axes.R
@@ -1,58 +1,58 @@
-test_that("axis defaults run without warning", {
-	expect_no_warning(scale_x_initial_fraction())
-	expect_no_warning(scale_x_initial_ratio())
-	expect_no_warning(scale_y_fitness())
-	expect_no_warning(scale_y_fitness_total())
-	expect_no_warning(scale_y_fitness_ratio())
+test_that("axis functions run with defaults", {
+	expect_no_error(scale_x_initial_fraction())
+	expect_no_error(scale_x_initial_ratio())
+	expect_no_error(scale_y_fitness())
+	expect_no_error(scale_y_fitness_total())
+	expect_no_error(scale_y_fitness_ratio())
 })
 
 test_that("axes accept expression() names", {
-	expect_no_warning(scale_x_initial_fraction(name = expression(x)))
-	expect_no_warning(scale_x_initial_ratio(name = expression(x)))
-	expect_no_warning(scale_y_fitness(name = expression(x)))
-	expect_no_warning(scale_y_fitness_total(name = expression(x)))
-	expect_no_warning(scale_y_fitness_ratio(name = expression(x)))
+	expect_no_error(scale_x_initial_fraction(name = expression(x)))
+	expect_no_error(scale_x_initial_ratio(name = expression(x)))
+	expect_no_error(scale_y_fitness(name = expression(x)))
+	expect_no_error(scale_y_fitness_total(name = expression(x)))
+	expect_no_error(scale_y_fitness_ratio(name = expression(x)))
 })
 
 test_that("axes accept specified breaks", {
-	expect_no_warning(scale_x_initial_fraction(breaks = c(0, 0.5, 1)))
+	expect_no_error(scale_x_initial_fraction(breaks = c(0, 0.5, 1)))
 	log_breaks <- c(0.1, 1, 10, 30)
-	expect_no_warning(scale_x_initial_ratio(breaks = log_breaks))
-	expect_no_warning(scale_y_fitness(breaks = log_breaks))
-	expect_no_warning(scale_y_fitness_total(breaks = log_breaks))
-	expect_no_warning(scale_y_fitness_ratio(breaks = log_breaks))
+	expect_no_error(scale_x_initial_ratio(breaks = log_breaks))
+	expect_no_error(scale_y_fitness(breaks = log_breaks))
+	expect_no_error(scale_y_fitness_total(breaks = log_breaks))
+	expect_no_error(scale_y_fitness_ratio(breaks = log_breaks))
 })
 
 test_that("axes accept specified break labels", {
-	expect_no_warning(scale_x_initial_fraction(
+	expect_no_error(scale_x_initial_fraction(
 		breaks = c(0, 0.5, 1), labels = c("Only A", "A+B", "Only B")
 	))
 	log_breaks <- c(0.1, 1, 10)
 	log_labels <- c("low", "medium", "high")
-	expect_no_warning(
+	expect_no_error(
 		scale_x_initial_ratio(breaks = log_breaks, labels = log_labels)
 	)
-	expect_no_warning(
+	expect_no_error(
 		scale_y_fitness(breaks = log_breaks, labels = log_labels)
 	)
-	expect_no_warning(
+	expect_no_error(
 		scale_y_fitness_total(breaks = log_breaks, labels = log_labels)
 	)
-	expect_no_warning(
+	expect_no_error(
 		scale_y_fitness_ratio(breaks = log_breaks, labels = log_labels)
 	)
 })
 
 test_that("axes accept specified minor_breaks", {
-	expect_no_warning(
+	expect_no_error(
 		scale_x_initial_fraction(minor_breaks = seq(0, 1, by = 0.1))
 	)
 	log_minor_breaks <- 3 * 10^{-2:2}
-	expect_no_warning(scale_x_initial_ratio(minor_breaks = log_minor_breaks))
-	expect_no_warning(scale_y_fitness(minor_breaks = log_minor_breaks))
-	expect_no_warning(scale_y_fitness_total(minor_breaks = log_minor_breaks))
-	expect_no_warning(scale_y_fitness_ratio(minor_breaks = log_minor_breaks))
-	expect_no_warning(scale_y_fitness(minor_breaks = NULL))
-	expect_no_warning(scale_y_fitness_total(minor_breaks = NULL))
-	expect_no_warning(scale_y_fitness_ratio(minor_breaks = NULL))
+	expect_no_error(scale_x_initial_ratio(minor_breaks = log_minor_breaks))
+	expect_no_error(scale_y_fitness(minor_breaks = log_minor_breaks))
+	expect_no_error(scale_y_fitness_total(minor_breaks = log_minor_breaks))
+	expect_no_error(scale_y_fitness_ratio(minor_breaks = log_minor_breaks))
+	expect_no_error(scale_y_fitness(minor_breaks = NULL))
+	expect_no_error(scale_y_fitness_total(minor_breaks = NULL))
+	expect_no_error(scale_y_fitness_ratio(minor_breaks = NULL))
 })
--- tests/testthat/test-plot-fitness.R
+++ tests/testthat/test-plot-fitness.R
@@ -1,19 +1,56 @@
-test_that("plot functions run defaults without warning", {
+test_that("plot functions run defaults", {
 	fitness_myxo <- calculate_mix_fitness(data_smith_2010, var_names_smith_2010)
-	expect_no_warning(plot_strain_fitness(fitness_myxo))
-	expect_no_warning(plot_total_group_fitness(fitness_myxo))
-	expect_no_warning(plot_within_group_fitness(fitness_myxo))
+	expect_no_error(plot_mix_fitness(fitness_myxo))
+	expect_no_error(plot_strain_fitness(fitness_myxo))
+	expect_no_error(plot_total_group_fitness(fitness_myxo))
+	expect_no_error(plot_within_group_fitness(fitness_myxo))
+})
+
+test_that("plot functions accept mixing ratio x-axis", {
+	fitness_myxo <- calculate_mix_fitness(data_smith_2010, var_names_smith_2010)
+	expect_no_error(plot_mix_fitness(fitness_myxo, mix_scale = "ratio"))
+	expect_no_error(plot_strain_fitness(fitness_myxo, mix_scale = "ratio"))
+	expect_no_error(plot_total_group_fitness(fitness_myxo, mix_scale = "ratio"))
+	expect_no_error(plot_within_group_fitness(fitness_myxo, mix_scale = "ratio"))
+})
+
+test_that("plot_mix_fitness() accepts single mixing scale", {
+	fitness_myxo <- calculate_mix_fitness(data_smith_2010, var_names_smith_2010)
+	expect_no_error(plot_mix_fitness(fitness_myxo, mix_scale = "fraction"))
+	expect_no_error(plot_mix_fitness(fitness_myxo, mix_scale = "ratio"))
 })
 
 test_that("plot functions accept expression() axis labels", {
 	fitness_myxo <- calculate_mix_fitness(data_smith_2010, var_names_smith_2010)
-	expect_no_warning(plot_strain_fitness(
-		fitness_myxo, xlab = expression(x), ylab = expression(y)
+	label <- expression(label)
+	expect_no_error(plot_strain_fitness(
+		fitness_myxo, xlab = label, ylab = label
 	))
-	expect_no_warning(plot_total_group_fitness(
-		fitness_myxo, xlab = expression(x), ylab = expression(y)
+	expect_no_error(plot_total_group_fitness(
+		fitness_myxo, xlab = label, ylab = label
 	))
-	expect_no_warning(plot_within_group_fitness(
-		fitness_myxo, xlab = expression(x), ylab = expression(y)
+	expect_no_error(plot_within_group_fitness(
+		fitness_myxo, xlab = label, ylab = label
 	))
 })
+
+test_that("plot functions accept point args", {
+	fitness_myxo <- calculate_mix_fitness(data_smith_2010, var_names_smith_2010)
+	color <- c("black", "grey55", "grey35")
+	fill <- c("grey45", "white", "grey75")
+	shape <- 23
+	size <- 2
+	expect_no_error(plot_mix_fitness(
+		fitness_myxo, color = color, fill = fill, shape = shape, size = size
+	))
+	expect_no_error(plot_strain_fitness(
+		fitness_myxo, color = color, fill = fill, shape = shape, size = size
+	))
+	expect_no_error(plot_total_group_fitness(
+		fitness_myxo, color = color, fill = fill, shape = shape, size = size
+	))
+	expect_no_error(plot_within_group_fitness(
+		fitness_myxo, color = color, fill = fill, shape = shape, size = size
+	))
+})
+
